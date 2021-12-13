a6691e24b4ed9cb0b9225e05b4a2bc3152e137fd
commGom <hyunwoo921129@gmail.com>
2021-12-13 19:11:42 +0900

    DOM event(element, window) click, mouseenter, mouseleave, resize, copy, online, offline

---
+++ js/dom.js
@@ -0,0 +1,36 @@
+// document 객체 : javascript 가 HTML DOM 에 접근 가능하다.
+// Event 처리 addEventListener("이벤트명",콜백함수);
+// ex) document element event : click, mouseenter, mouseleave
+// ex) window event : resize, copy
+
+const title = document.querySelector("h1");
+console.log(title);
+console.log(title.innerText);
+title.innerText = "Got you!";
+function handleTitleClick() {
+    title.style.color = 'red';
+    console.log('title was clicked');
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+    alert('copied');
+}
+title.onclick = handleTitleClick;
+title.addEventListener('mouseenter', () => {
+    console.log("mouse Entered");
+})
+title.addEventListener('mouseleave', () => {
+    console.log("mouse leave");
+})
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", () => {
+    alert('SOS No wifi');
+})
+window.addEventListener("online", () => {
+    alert('All Good');
+})
